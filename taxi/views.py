250f3053048e8474e8cbe2de24f84e501e60cf2a
Lukian <lukian55d@gmail.com>
2026-03-08 21:25:25 +0000

    flake8

--- taxi/views.py
+++ taxi/views.py
@@ -6,7 +6,13 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Driver, Car, Manufacturer
-from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm, DriverSearchForm, CarSearchForm
+from .forms import (
+    DriverCreationForm,
+    DriverLicenseUpdateForm,
+    CarForm,
+    DriverSearchForm,
+    CarSearchForm
+)
 
 
 @login_required
